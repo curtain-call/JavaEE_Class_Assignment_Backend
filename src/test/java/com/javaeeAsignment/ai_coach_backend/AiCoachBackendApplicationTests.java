6cfca05b99eb06e54ec1351d1512108f24c44437
curtaincall <curtaincall_99@163.com>
2023-11-14 17:23:43 +0800

    first commit

---
+++ src/test/java/com/javaeeAsignment/ai_coach_backend/AiCoachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeeAsignment.ai_coach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCoachBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
